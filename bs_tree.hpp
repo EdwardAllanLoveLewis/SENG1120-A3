113184165716b5821dc48e9a0dfa933cda2b3891
Edward Lewis <edwardallanlovelewis@gmail.com>
2024-06-05 11:47:35 +1000

    Update bs_tree.hpp
    yes

--- bs_tree.hpp
+++ bs_tree.hpp
@@ -7,32 +7,37 @@
  */
 
 template <typename T>
-BSTree<T>::BSTree(){
+BSTree<T>::BSTree()
+{
     root = nullptr;
     count = 0;
 }
 
 template <typename T>
-BSTree<T>::~BSTree(){
+BSTree<T>::~BSTree()
+{
     this->clear();
     root = nullptr;
     count = 0;
 }
 
 template <typename T>
-void BSTree<T>::insert(const T &data){
+void BSTree<T>::insert(const T &data)
+{
     root = insert(data, root);
 }
 
 template <typename T>
-BTNode<T>* BSTree<T>::insert(const T& data, BTNode<T>* node){
-    if(!node){
+BTNode<T> *BSTree<T>::insert(const T &data, BTNode<T> *node)
+{
+    if (!node)
+    {
         count++;
         return new BTNode<T>(data);
     }
-    else if(data < node->get_data())
+    else if (data < node->get_data())
         node->set_left(insert(data, node->get_left()));
-    else if(data > node->get_data())
+    else if (data > node->get_data())
         node->set_right(insert(data, node->get_right()));
     else
         node->set_data(data);
@@ -41,114 +46,144 @@ BTNode<T>* BSTree<T>::insert(const T& data, BTNode<T>* node){
 } // Helper Method
 
 template <typename T>
-void BSTree<T>::remove(const std::string &key){
+void BSTree<T>::remove(const std::string &key)
+{
     return remove(key, root);
 }
 
 template <typename T>
-BTNode<T>* BSTree<T>::remove(const std::string& key, BTNode<T>* node){
+BTNode<T> *BSTree<T>::remove(const std::string &key, BTNode<T> *node)
+{
+    if (node->get_left() == nullptr && node->get_right() == nullptr)
+    {
+        if (node->get_data() == key)
+        {
+            node->delete node;
+            // Set parent pointers to nullptr
+        }
+        if (node->get_left == key &&)
+    }
+    else if
 
-} //Helper Method
+} // Helper Method
 
 template <typename T>
-bool BSTree<T>::contains(const std::string &key) const{
+bool BSTree<T>::contains(const std::string &key) const
+{
     return contains(key, root);
 }
 
 template <typename T>
-bool BSTree<T>::contains(const std::string& key, BTNode<T>* node) const{
-    if(!node)
+bool BSTree<T>::contains(const std::string &key, BTNode<T> *node) const
+{
+    if (!node)
         return false;
-    if(node->get_data().get_key() == key)
+    if (node->get_data().get_key() == key)
         return true;
-    if(key < node->get_data().get_key())
+    if (key < node->get_data().get_key())
         return contains(key, node->get_left());
-    if(key > node->get_data().get_key())
+    if (key > node->get_data().get_key())
         return contains(key, node->get_right());
 } // Helper Method
 
 template <typename T>
-void BSTree<T>::clear(){
+void BSTree<T>::clear()
+{
     return clear(root);
 }
 
 template <typename T>
-void BSTree<T>::clear(BTNode<T>* root){
-
+void BSTree<T>::clear(BTNode<T> *node)
+{
+    if (!node)
+    {
+    }
 } // Helper method
 
 template <typename T>
-T *BSTree<T>::find(const std::string &key) const{
+T *BSTree<T>::find(const std::string &key) const
+{
     return find(key, root);
 }
 
 template <typename T>
-BTNode<T>* BSTree<T>::find(const std::string& key, BTNode<T>* node) const{
-    if(!node)
+BTNode<T> *BSTree<T>::find(const std::string &key, BTNode<T> *node) const
+{
+    if (!node)
         return nullptr;
-    if(node->get_data().get_key() == key)
+    if (node->get_data().get_key() == key)
         return node->get_data();
-    if(key < node->get_data().get_key())
+    if (key < node->get_data().get_key())
         return find(key, node->get_left());
-    if(key > node->get_data().get_key())
+    if (key > node->get_data().get_key())
         return find(key, node->get_right());
 } // Helper Method
 
 template <typename T>
-T &BSTree<T>::find_min() const{
-    if(empty() || !root)
+T &BSTree<T>::find_min() const
+{
+    if (empty() || !root)
         throw empty_collection_exception();
 
     return find_min(root)->get_data();
 }
 
 template <typename T>
-BTNode<T>* BSTree<T>::find_min(BTNode<T>* node) const{
-    if(!node->get_left())
+BTNode<T> *BSTree<T>::find_min(BTNode<T> *node) const
+{
+    if (!node->get_left())
         return node;
 
     return find_min(node->get_left());
 } // Helper Method
 
 template <typename T>
-T &BSTree<T>::find_max() const{
-    if(empty() || !root)\
+T &BSTree<T>::find_max() const
+{
+    if (empty() || !root)
         throw empty_collection_exception();
 
     return find_max(root)->get_data();
 }
 
 template <typename T>
-BTNode<T>* BSTree<T>::find_max(BTNode<T>* node) const{
-    if(!node->get_right())
+BTNode<T> *BSTree<T>::find_max(BTNode<T> *node) const
+{
+    if (!node->get_right())
         return node;
 
     return find_max(node->get_right());
 } // Helper Method
 
 template <typename T>
-bool BSTree<T>::empty() const{
-    if(count == 0){
+bool BSTree<T>::empty() const
+{
+    if (count == 0)
+    {
         return true;
     }
-    else {
+    else
+    {
         return false;
     }
 }
 
 template <typename T>
-int BSTree<T>::size() const{
+int BSTree<T>::size() const
+{
     return count;
 }
 
 template <typename T>
-void BSTree<T>::print_inorder(std::ostream &out) const{
+void BSTree<T>::print_inorder(std::ostream &out) const
+{
     print_inorder(out, root);
 }
 
 template <typename T>
-void BSTree<T>::print_inorder(std::ostream& out, BTNode<T>* node) const{
-    if(node)
+void BSTree<T>::print_inorder(std::ostream &out, BTNode<T> *node) const
+{
+    if (node)
     {
         print_inorder(out, node->get_left());
         out << node->get_data() << " ";
@@ -157,13 +192,15 @@ void BSTree<T>::print_inorder(std::ostream& out, BTNode<T>* node) const{
 } // Helper Method
 
 template <typename T>
-void BSTree<T>::print_preorder(std::ostream &out) const{
+void BSTree<T>::print_preorder(std::ostream &out) const
+{
     print_preorder(out, root);
 }
 
 template <typename T>
-void BSTree<T>::print_preorder(std::ostream& out, BTNode<T>* node) const{
-    if(node)
+void BSTree<T>::print_preorder(std::ostream &out, BTNode<T> *node) const
+{
+    if (node)
     {
         out << node->get_data() << " ";
         print_preorder(out, node->get_left());
@@ -172,13 +209,15 @@ void BSTree<T>::print_preorder(std::ostream& out, BTNode<T>* node) const{
 } // Helper Method
 
 template <typename T>
-void BSTree<T>::print_postorder(std::ostream &out) const{
+void BSTree<T>::print_postorder(std::ostream &out) const
+{
     print_postorder(out, root);
 }
 
 template <typename T>
-void BSTree<T>::print_postorder(std::ostream& out, BTNode<T>* node) const{
-    if(node)
+void BSTree<T>::print_postorder(std::ostream &out, BTNode<T> *node) const
+{
+    if (node)
     {
         print_postorder(out, node->get_left());
         print_postorder(out, node->get_right());
@@ -187,8 +226,9 @@ void BSTree<T>::print_postorder(std::ostream& out, BTNode<T>* node) const{
 } // Helper Method
 
 template <typename T>
-std::ostream& operator<<(std::ostream& out, const BSTree<T>& tree){
-    if(tree.empty())
+std::ostream &operator<<(std::ostream &out, const BSTree<T> &tree)
+{
+    if (tree.empty())
     {
         out << "Empty tree" << std::endl;
     }
@@ -198,17 +238,3 @@ std::ostream& operator<<(std::ostream& out, const BSTree<T>& tree){
     }
     return out;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
